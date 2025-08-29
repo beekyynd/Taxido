098eba3fbec36cc71f52b26900725bed4e3b5c20
Francis Kalu <snapchainx@gmail.com>
2025-08-29 01:04:10 +0100

    new

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import { registerRootComponent } from 'expo';
+/**
+ * @format
+ */
 
+import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
+import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+messaging().setBackgroundMessageHandler(async remoteMessage => {    
+    // Not a Responsys Push, your app should handle this notification
+});
+
+LogBox.ignoreAllLogs();
+AppRegistry.registerComponent(appName, () => App);
